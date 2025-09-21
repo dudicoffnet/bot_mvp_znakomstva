143286a6a387ceb7154e379c224164d97b2fbd75
dudicoffnet <168195277+dudicoffnet@users.noreply.github.com>
2025-09-21 22:15:52 +0300

    Delete handlers.py

--- handlers.py
+++
@@ -1,7 +0,0 @@
-
-from aiogram import Dispatcher, types
-
-def register_handlers(dp: Dispatcher):
-    @dp.message_handler(commands=["start"])
-    async def send_welcome(message: types.Message):
-        await message.answer("Привет! Это тестовый бот знакомств.")
